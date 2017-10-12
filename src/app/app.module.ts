1d49e28e7c3c96cf930a774f4ad48c6b98406286
Faraz Aftab <farazaftab@Farazs-MacBook-Pro.local>
2017-10-11 21:43:08 -0400

    test

---
+++ src/app/app.module.ts
@@ -0,0 +1,46 @@
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
+
+import {APP_CONFIG, AppConfig} from './config/app.config';
+
+import {AppRoutingModule} from './app-routing.module';
+import {SharedModule} from './shared/modules/shared.module';
+import {CoreModule} from './core/core.module';
+
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
+import {HttpLoaderFactory} from './app.translate.factory';
+import {HeroTopComponent} from './heroes/hero-top/hero-top.component';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    }),
+    SharedModule.forRoot(),
+    CoreModule,
+    AppRoutingModule
+  ],
+  declarations: [
+    AppComponent,
+    HeroTopComponent
+  ],
+  providers: [
+    {provide: APP_CONFIG, useValue: AppConfig}
+  ],
+  bootstrap: [AppComponent]
+})
+
+export class AppModule {
+}
---
+++ src/app/shared/modules/shared.module.ts
@@ -0,0 +1,28 @@
+import {ModuleWithProviders, NgModule} from '@angular/core';
+import {ProgressBarService} from '../services/progress-bar.service';
+import {MaterialModule} from './material.module';
+import {TranslateModule} from '@ngx-translate/core';
+import {HeroService} from '../../heroes/shared/hero.service';
+import {UtilsService} from '../services/utils.service';
+
+@NgModule({
+  imports: [
+    MaterialModule,
+    TranslateModule
+  ],
+  exports: [
+    MaterialModule,
+    TranslateModule
+  ]
+})
+
+export class SharedModule {
+  static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: SharedModule,
+      providers: [
+        ProgressBarService, HeroService, UtilsService
+      ]
+    };
+  }
+}
