cfa401b92a46b8340c1f05b1fec3916168fcd244
mfaraz <mohammad.faraz@dcsg.com>
2017-10-13 19:25:01 -0400

    commit

---
+++ src/app/school/shared/schoolfeature.model.ts
@@ -0,0 +1,7 @@
+export class SchoolFeature {
+  constructor(public cardName: string,
+              public img: string,
+              public para: string,
+              public likes: string) {
+  }
+}
---
+++ src/app/school/shared/schoolfeature.service.ts
@@ -0,0 +1,48 @@
+import {EventEmitter, Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+
+import {AppConfig} from '../../config/app.config';
+
+import {SchoolFeature} from './schoolfeature.model';
+import {Observable} from 'rxjs/Observable';
+import {MatSnackBar, MatSnackBarConfig} from '@angular/material';
+import {TranslateService} from '@ngx-translate/core';
+
+@Injectable()
+export class SchoolFeatureService {
+  request$: EventEmitter<any>;
+
+  private headers: HttpHeaders;
+  private heroesUrl: string;
+
+  private handleError(error: any) {
+    this.request$.emit('finished');
+    if (error instanceof Response) {
+      return Observable.throw(error.json()['error'] || 'backend server error');
+    }
+    return Observable.throw(error || 'backend server error');
+  }
+
+  constructor(private http: HttpClient,
+              private translateService: TranslateService,
+              private snackBar: MatSnackBar) {
+    this.request$ = new EventEmitter();
+
+    this.heroesUrl = AppConfig.endpoints.heroes;
+    this.headers = new HttpHeaders({'Content-Type': 'application/json'});
+
+  }
+
+  getAllHeroes(): Observable<SchoolFeature[]> {
+
+    this.request$.emit('starting');
+    return this.http.get('../../assets/resjson/feature.json')
+        .map(response => {
+          this.request$.emit('finished');
+          return response;
+        })
+        .catch(error => this.handleError(error));
+  }
+
+
+}
