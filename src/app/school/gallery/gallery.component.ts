7518047c01a294f5d467619c4534527697f91e19
farazaftab <farazaftab@yahoo.com>
2017-10-17 12:44:09 -0400

    commit

---
+++ src/app/school/gallery/gallery.component.ts
@@ -0,0 +1,38 @@
+import {Component} from '@angular/core';
+
+
+@Component({
+  selector: 'app-gallery',
+  templateUrl: './gallery.component.html',
+  styleUrls: ['./gallery.component.scss']
+})
+export class GalleryComponent {
+
+
+    constructor() {
+
+    }
+  /*heroes: Hero[] = null;
+  canVote = false;
+
+  constructor(private heroService: HeroService) {
+    this.canVote = this.heroService.checkIfUserCanVote();
+
+    this.heroService.getAllHeroes().subscribe((heroes) => {
+      this.heroes = heroes.sort((a, b) => {
+        return b.likes - a.likes;
+      }).slice(0, AppConfig.topHeroesLimit);
+    });
+  }
+
+  like(hero: Hero): Promise<any> {
+    return new Promise((resolve, reject) => {
+      this.heroService.like(hero).subscribe(() => {
+        this.canVote = this.heroService.checkIfUserCanVote();
+        resolve(true);
+      }, (error) => {
+        reject(error);
+      });
+    });
+  }*/
+}
